af2137f56711704703ba3ccde1b02c8473798254
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-08-03 14:59:23 -0500

    example on classes

---
+++ people_class.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, name, email, phone):
+         self.name = name
+         self.email = email
+         self.phone = phone
+
+    def greet(self, other_person):
+        print('Hello %s, I am %s!' % (other_person.name, self.name))
+    
+    def contact (self,email,phone):
+        print("Email: %s Phone: %s" % (email, phone))
+
+
+# Instantiate an instance object of Person with name of 'Sonny', email of 'sonny@hotmail.com', and phone of '483-485-4948', store it in the variable sonny.
+sonny = Person("Sonny","sonny@hotmail.com","483-485-4948")
+
+# Instantiate another person with the name of 'Jordan', email of 'jordan@aol.com', and phone of '495-586-3456', store it in the variable 'jordan'.
+jordan = Person("Jordan","jordan@aol.com","495-586-3456")
+# Have sonny greet jordan using the greet method.
+sonny.greet(jordan)
+# Have jordan greet sonny using the greet method.
+jordan.greet(sonny)
+# Write a print statement to print the contact info (email and phone) of Sonny.
+sonny.contact(sonny.email,sonny.phone)
+# Write another print statement to print the contact info of Jordan.
+jordan.contact(jordan.email,jordan.phone)
